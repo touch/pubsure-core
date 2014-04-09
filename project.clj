3f94cf57ffbf299ce4a45a01c7911b2e10e2fc99
aroemers <github@company.romeinszoon.nl>
2014-04-09 11:55:24 +0200

    Add clojure maven plugin to project.

--- project.clj
+++ project.clj
@@ -4,4 +4,18 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]])
+                 [org.clojure/core.async "0.1.278.0-76b25b-alpha"]]
+  :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.3.15"
+                 {:extensions "true"
+                  :executions ([:execution
+                                [:id "clojure-compile"]
+                                [:phase "compile"]
+                                [:configuration
+                                 [:temporaryOutputDirectory "true"]
+                                 [:sourceDirectories [:sourceDirectory "src"]]]
+                                [:goals [:goal "compile"]]]
+                                 [:execution
+                                  [:id "clojure-test"]
+                                  [:phase "test"]
+                                  [:goals [:goal "test"]]])}]]
+  :pom-addition [:properties [:project.build.sourceEncoding "UTF-8"]])
