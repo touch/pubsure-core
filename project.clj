01f0fc0672a30e82b0d4331ba9885d8c4313b567
Misha Vakulich <m.vakulich@softteco.com>
2023-09-07 14:07:10 +0300

    up clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
                  [org.clojure/core.async "1.5.640"]
                  [com.taoensso/timbre "6.2.2"]]
   :pom-plugins [[com.theoryinpractise/clojure-maven-plugin "1.7.1"
